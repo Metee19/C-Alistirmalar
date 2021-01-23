e088c96ff880ee700b103f2fd7814f0d00cd021e
Metee19 <77540484+Metee19@users.noreply.github.com>
2021-01-23 23:51:26 +0300

    Examples

---
+++ OOP2/MusterManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{   //İş sınıfları // business classes
+    class MusterManager
+    {   
+        public void Add(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Ad = "Mete";
+            musteri1.Soyad = "Ceyhan";
+            musteri1.TcNo = "11111111111";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "22345";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusterManager musterManager = new MusterManager();
+            musterManager.Add(musteri1);
+            musterManager.Add(musteri2);
+            
+
+        }
+    }
+}
